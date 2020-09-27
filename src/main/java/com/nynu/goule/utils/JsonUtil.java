8e9b9b0797fc014740dbcd1dc88fcad971114109
goule <1179061082@qq.com>
2020-09-27 11:00:23 +0800

    工具类更新

--- src/main/java/com/nynu/goule/utils/JsonUtil.java
+++ src/main/java/com/nynu/goule/utils/JsonUtil.java
@@ -1,4 +1,88 @@
 package com.nynu.goule.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
+import org.slf4j.LoggerFactory;
+import com.google.common.collect.Maps;
+import org.springframework.cglib.beans.BeanMap;
+import org.springframework.util.CollectionUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/**
+ * json转换工具类
+ *
+ */
 public class JsonUtil {
+
+    private static ObjectMapper objectMapper;
+    private static Logger LOGGER = (Logger) LoggerFactory.getLogger(JsonUtil.class);
+    private static Map convertorMap = new HashMap<>();
+
+    static {
+        objectMapper = new ObjectMapper();
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-mm-dd HH:MM:SS"));
+    }
+
+    private JsonUtil(){}
+
+    /**
+     * 将object对象转换为json对象
+     */
+    public static String convertObject2Json(Object object){
+        String method = "convertObject2Json";
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 将json对象转换为Object
+     */
+    public static Object convertJson2Object(String json, Class cls){
+        String method = "convertJson2Object";
+        try {
+            return objectMapper.readValue(json, cls);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 将List<T>转换为List<Map<String, Object>>
+     */
+    public static <T> List<Map<String, Object>> objects2Maps(List<T> objectList){
+        List<Map<String, Object>> list = Lists.newArrayList();
+        if(!CollectionUtils.isEmpty(objectList)) {
+            for(int i = 0, size = objectList.size(); i< size; i++){
+                T bean = objectList.get(i);
+                Map<String, Object> map = bean2Map(bean);
+                list.add(map);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 将对象转换成map
+     */
+    public static <T> Map<String, Object> bean2Map(T bean) {
+        Map<String, Object> map = Maps.newHashMap();
+        if(bean != null) {
+            BeanMap beanMap = BeanMap.create(bean);
+            for(Object key : beanMap.keySet()){
+                map.put(key + "", beanMap.get(key));
+            }
+        }
+        return map;
+    }
 }
--- src/main/java/com/nynu/goule/utils/StringUtil.java
+++ src/main/java/com/nynu/goule/utils/StringUtil.java
@@ -1,5 +1,12 @@
 package com.nynu.goule.utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 字符串工具类
  */
@@ -9,6 +16,118 @@ public class StringUtil {
     }
 
     /**
-     *
+     *判断字符串是否非null && 非空字符
      */
+    public static boolean isNotEmpty(String param){
+        return param != null && !"".equals(param.trim());
+    }
+
+    /**
+     * 判断字符串是否为null || 空字符串
+     */
+    public static boolean isEmpty(String param){
+        return param == null || "".equals(param.trim());
+    }
+
+    /**
+     * 判断对象是否为null || 空字符串
+     */
+    public static boolean isEmpty(Object obj){
+        if(obj == null){
+            return true;
+        }else{
+            return "".equals(obj.toString().trim());
+        }
+    }
+
+    /**
+     * true = is num
+     */
+    public static boolean isNum(String str){
+        String regex = "^(-?\\d+)(\\.\\d+)?$";
+        return matchRegex(str, regex);
+    }
+
+    private static boolean matchRegex(String value, String regex){
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(value);
+        return matcher.matches();
+    }
+
+    /**
+     * 获取字符串的值
+     */
+    public static String getStringValue(Object object){
+        if(object != null){
+            return String.valueOf(object);
+        }
+        return "";
+    }
+
+    /**
+     * 根据key获取value
+     */
+    public static String getString(Map inputParam, String key){
+        String resultValue = null;
+        if(null != inputParam && null != inputParam.get(key)){
+            resultValue = String.valueOf(inputParam.get(key));
+        }
+        return resultValue;
+    }
+
+    /**
+     * 获取两个List的不同元素
+     */
+    private static List<String> getDiffrent(List<String> list1, List<String> list2){
+        List<String> diff = new ArrayList<>();
+        for(String str: list1){
+            if (!list2.contains(str)) {
+                diff.add(str);
+            }
+        }
+        return diff;
+    }
+
+    /**
+     * 获取两个List的相同元素
+     */
+    private static List<String> getEqualDiffrent(List<String> list1, List<String> list2){
+        List<String> diff = new ArrayList<>();
+        for(String str: list1){
+            if (list2.contains(str)) {
+                diff.add(str);
+            }
+        }
+        return diff;
+    }
+
+    /**
+     * 获取数据的不同元素
+     */
+    private static List<Object> getDiffrent(Object[] str1, Object[] str2){
+        List<Object> list1 = Arrays.asList(str1);
+        List<Object> list2 = Arrays.asList(str1);
+        List<Object> diff = new ArrayList<>();
+        for(Object str: list1){
+            if (list2.contains(str)) {
+                diff.add(str);
+            }
+        }
+        return diff;
+    }
+
+    /**
+     * 获取数据的相同元素
+     */
+    private static List<Object> getNotEqualDiffrent(Object[] str1, Object[] str2){
+        List<Object> list1 = Arrays.asList(str1);
+        List<Object> list2 = Arrays.asList(str1);
+        List<Object> diff = new ArrayList<>();
+        for(Object str: list1){
+            if (!list2.contains(str)) {
+                diff.add(str);
+            }
+        }
+        return diff;
+    }
 }
